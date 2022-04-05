2f8fce06208d452d857b9ee41b1d13952f029e3c
Thierry <thierry.martins.r.s@gmail.com>
2022-04-05 18:30:17 -0400

    finalizando trainer.py

---
+++ projetofinal769/trainer.py
@@ -0,0 +1,11 @@
+class Trainer():
+    def __init__():
+        pass
+    
+    def trainer_mega():
+        pipeline = []
+        model = []
+        final_pipe = [ ]
+        #fit
+        #Evaluate
+        return final_pipe
